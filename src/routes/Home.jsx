d25447c96f0044ac1036a74bc445eab490fd9191
Agustin Sirotinsky <agussiro06@gmail.com>
2023-11-07 21:32:54 -0300

    imagenes ready

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,28 +2,48 @@
 import { Link } from "react-router-dom";
 import { React, useEffect, useState } from "react";
 
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 //Other
 import proyectos from "../components/proyectos.json"; //Importar el JSON de proyectos
+//Importar imagenes
+import StudentBites from "../images/StudentBites.png"
+import EcoQuiz from "../images/EcoQuiz.png"
+
 
 export default function Home() { 
-
+  let ImagenesArray = [StudentBites, EcoQuiz];	
     useEffect(() => {
-        console.log(proyectos);
+        console.log(ImagenesArray);
     }, [])
 
     return (
-        <div>
+      <Container>
       <h1>Project List</h1>
-      <ul>
+      <Row>
         {proyectos.map((project) => (
-          <li key={project.ID}>
-            <h2>{project.Nombre}</h2>
-            <p>{project.Descripcion}</p>
-            <p>Fecha: {project.Fecha}</p>
-            <p>Tecnologias: {project.Tecnologias}</p>
-          </li>
+          <Col key={project.ID} xs={12} sm={6} md={4} lg={3}>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={ImagenesArray[project.ID]}
+                alt={project.Nombre}
+              />
+              <Card.Body>
+                <Card.Title>{project.Nombre}</Card.Title>
+                <Card.Text>{project.Descripcion}</Card.Text>
+                <Card.Text>Fecha: {project.Fecha}</Card.Text>
+                <Card.Text>Tecnologias: {project.Tecnologias}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
-    </div>
+      </Row>
+    </Container>
     )
 }
